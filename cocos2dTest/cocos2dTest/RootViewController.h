ecdb796b48e72fefcabd231bfd6286039f7461ad
Benjamin Lee <hullabal00@yahoo.com>
2011-08-04 01:05:29 -0700

    cocos2dTest project added

---
+++ cocos2dTest/cocos2dTest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  cocos2dTest
+//
+//  Created by Benjamin Lee on 8/4/11.
+//  Copyright Cisco 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
